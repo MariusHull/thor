5d92ae538843c199f915b6de50ca336b25f03668
MariusHull <marius.hullin@gmail.com>
2019-04-12 00:27:24 +0100

    updates

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
-// import axios from "axios";
+import axios from "axios";
 import "./Dashboard.scss";
+import Loader from "react-loader-spinner";
 
 class DashBoard extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      data: ["test"],
       sites: [
-        { siteName: "TEST", siteUrl: "test/test" },
+        { siteName: "SELF", siteUrl: "localhost:4242" },
+        {
+          siteName: "TEST",
+          siteUrl: "http://prototype.centralesupelec.fr/back/api/users"
+        },
         { siteName: "TEST2", siteUrl: "test/test" },
         { siteName: "TEST3", siteUrl: "test/test" }
       ]
@@ -24,8 +30,23 @@ class DashBoard extends Component {
     return 0;
   };
 
+  ping = adress => {
+    axios
+      .get(adress)
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(error => {
+        console.log("Erreur : " + error);
+        this.setState({ data: "Erreur" + error });
+        if (error.response) {
+          console.log(error.response.status);
+        }
+      });
+  };
+
   render() {
-    const { sites } = this.state;
+    const { sites, data } = this.state;
     return (
       <div className="container">
         <div class="card text-white bg-dark">
@@ -36,6 +57,17 @@ class DashBoard extends Component {
                 <div class="card-body">
                   <h5 class="card-title">{site.siteName}</h5>
                   <p class="card-text">Site URL : {site.siteUrl}</p>
+                  <p>{data}</p>
+                  <Loader type="Audio" color="#00BFFF" height="50" width="50" />
+                  <div className="row space">
+                    <button
+                      type="button"
+                      class="btn btn-outline-success col"
+                      onClick={() => this.ping(site.siteUrl)}
+                    >
+                      Ping this adress !
+                    </button>
+                  </div>
                 </div>
               ))}
             </div>
